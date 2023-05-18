eec1f63b902d785c1cf54af90e6001d416aed578
Shahadat171 <shahadathosan999@gmail.com>
2023-05-19 00:04:06 +0600

    Banner added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,63 +1,14 @@
-import banner1 from "../../../assets/banner-image/banner1.jpg";
-import banner2 from "../../../assets/banner-image/banner2.jpg";
-import banner3 from "../../../assets/banner-image/banner3.jpg";
-import banner4 from "../../../assets/banner-image/banner4.jpg";
+import Banner from "../Banner/Banner";
+import PhotoGallary from "../PhotoGallary/PhotoGallary";
+
 
 const Home = () => {
   return (
-    <div className="carousel h-fit w-fit mx-auto">
-      <div id="slide1" className="carousel-item relative w-full">
-        <div className="relative">
-          <div className="absolute top-[25%] left-[20%] right-[20%] text-center text-white text-7xl">
-            <h2>Pick The Best Toy ForYour Kid</h2>
-            <button className=" rounded-full btn text-7xl bg-[#c00bb1] h-5 w-1/6"> <span className="text-yellow-100">Read More</span> </button>
-          </div>
-          <img src={banner1} className="w-full" />
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={banner2} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={banner3} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={banner4} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+    <div>
+        <Banner></Banner>
+        <PhotoGallary></PhotoGallary>
     </div>
-  );
-};
+  )
+}
 
 export default Home;
---
+++ src/Pages/Home/PhotoGallary/PhotoGallary.jsx
@@ -0,0 +1,16 @@
+import './PhotoGallary.css'
+
+const PhotoGallary = () => {
+    return (
+        <div className='container text-center'>
+            <div>
+                <h2 className='pt-24 text-5xl font-serif font-bold'>Best Products</h2>
+                <div className=''>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PhotoGallary;
