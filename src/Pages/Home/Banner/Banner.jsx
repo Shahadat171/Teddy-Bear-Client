91509540cf3ff7b7fd36fdba08052d085ee2772a
Shahadat171 <shahadathosan999@gmail.com>
2023-05-20 00:02:54 +0600

    subcatory three card designed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,23 +1,32 @@
 import banner1 from "../../../assets/banner-image/banner1.jpg";
-import './Banner.css'
+import "./Banner.css";
 
 const Banner = () => {
-    // bg-gradient-to-r from-indigo-500 
-    // top-[25%] left-[20%] right-[20%]
-    return (
-      <div className="hero min-h-screen" style={{ backgroundImage: `url(${banner1})` }}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-8 text-5xl font-bold font-serif">Pick The Best Toy For
-Your Kid</h1>
-      <p className="mb-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="inline-flex rounded-full items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium">
-        View More</button>
+  // bg-gradient-to-r from-indigo-500
+  // top-[25%] left-[20%] right-[20%]
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url(${banner1})` }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-8 text-5xl font-bold font-serif">
+            Pick The Best Toy For Your Kid
+          </h1>
+          <p className="mb-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="inline-flex rounded-full items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium">
+            View More
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Banner;
--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import TeddyBearTab from "../TeddyBearTab/TeddyBearTab";
+import { FaDollarSign } from "react-icons/fa";
+import { Rating } from "@mui/material";
 
 const Category = () => {
   const [category, setCategory] = useState([]);
@@ -32,87 +33,146 @@ const Category = () => {
   };
 
   return (
-    <div className="w-2/3 mx-auto text-center mt-5 ">
-      <Tabs>
-        <TabList className='flex-grow bg-gray-200 pt-2 pb-4 rounded-md space-x-4'>
-          <Tab onClick={handleBlackbeardata}>
-            {" "}
-            <span className="font-serif text-xl">Black Teddy Bear</span>
-          </Tab>
-          <Tab onClick={handleGrizzlydata}>
-            {" "}
-            <span className="font-serif text-xl ">Grizzly Teddy Bear </span>
-          </Tab>
-          <Tab onClick={handlePandadata}>
-            {" "}
-            <span className="font-serif text-xl">Panda Teddy Bear </span>
-          </Tab>
-        </TabList>
+    <div className="">
+      <div className="w-2/3 mx-auto text-center pt-5 ">
+        <Tabs>
+          <TabList className="flex-grow bg-gray-200 pt-2 pb-4 rounded-md space-x-4">
+            <Tab onClick={handleBlackbeardata}>
+              {" "}
+              <span className="font-serif text-xl">Black Teddy Bear</span>
+            </Tab>
+            <Tab onClick={handleGrizzlydata}>
+              {" "}
+              <span className="font-serif text-xl ">Grizzly Teddy Bear </span>
+            </Tab>
+            <Tab onClick={handlePandadata}>
+              {" "}
+              <span className="font-serif text-xl">Panda Teddy Bear </span>
+            </Tab>
+          </TabList>
 
-        <TabPanel className=''>
-          <div className="flex gap-6 mt-5">
-            {Blackbear.map((blackTeddyBear) => (
-              <div
-                className="card w-96 bg-base-100 shadow-xl"
-                key={blackTeddyBear.id}
-              >
-                <figure>
-                  <img src={blackTeddyBear.image} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+          <TabPanel className="">
+            <div className="flex gap-6 mt-5 mb-5">
+              {Blackbear.map((blackTeddyBear) => (
+                <div
+                  style={{
+                    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                    maxHeight: "450px",
+                  }}
+                  className="card w-96  shadow-xl"
+                  key={blackTeddyBear.id}
+                >
+                  <figure className="bg-[#E6EFFF]">
+                    <img src={blackTeddyBear.image} />
+                  </figure>
+                  <div className="card-body bg-orange-200 text-center">
+                    <h2 className="text-center text-xl font-semibold font-serif ">
+                      {blackTeddyBear.name}
+                    </h2>
+                    <p className="text-center font-sans flex justify-center items-center font-semibold ">
+                      Price:
+                      <FaDollarSign className="" />
+                      {blackTeddyBear.price}
+                    </p>
+                    <Rating
+                      className="text-ceneter justify-center my-2"
+                      name="half-rating-read"
+                      defaultValue={blackTeddyBear.rating}
+                      precision={0.5}
+                      readOnly
+                    ></Rating>
+                    <div className="">
+                      <button className="inline-flex rounded-full items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium">
+                        View Details
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="flex gap-6 mt-5">
-            {Grizzlybear.map((grizzlyTeddyBear) => (
-              <div
-                className="card w-96 bg-base-100 shadow-xl"
-                key={grizzlyTeddyBear.id}
-              >
-                <figure>
-                  <img src={grizzlyTeddyBear.image} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="flex gap-6 mt-5">
+              {Grizzlybear.map((grizzlyTeddyBear) => (
+                <div
+                  style={{
+                    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                    maxHeight: "430px",
+                  }}
+                  className="card w-96 bg-base-100 shadow-xl"
+                  key={grizzlyTeddyBear.id}
+                >
+                  <figure className="bg-[#E6EFFF]">
+                    <img src={grizzlyTeddyBear.image} />
+                  </figure>
+                  <div className="card-body bg-orange-200 text-center">
+                    <h2 className="text-center text-xl font-semibold font-serif ">
+                      {grizzlyTeddyBear.name}
+                    </h2>
+                    <p className="text-center font-sans flex justify-center items-center font-semibold ">
+                      Price:
+                      <FaDollarSign className="" />
+                      {grizzlyTeddyBear.price}
+                    </p>
+                    <Rating
+                      className="text-ceneter justify-center my-2"
+                      name="half-rating-read"
+                      defaultValue={grizzlyTeddyBear.rating}
+                      precision={0.5}
+                      readOnly
+                    ></Rating>
+                    <div className="">
+                      <button className="inline-flex rounded-full items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium">
+                        View Details
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="flex gap-6 mt-5">
-            {Panda.map((pandaTeddyBear) => (
-              <div
-                className="card w-96 bg-base-100 shadow-xl"
-                key={pandaTeddyBear.id}
-              >
-                <figure>
-                  <img src={pandaTeddyBear.image} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="flex gap-6 mt-5">
+              {Panda.map((pandaTeddyBear) => (
+                <div
+                  style={{
+                    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                    maxHeight: "450px",
+                  }}
+                  className="card w-96 bg-base-100 shadow-xl"
+                  key={pandaTeddyBear.id}
+                >
+                  <figure className="bg-[#E6EFFF]">
+                    <img src={pandaTeddyBear.image} />
+                  </figure>
+                  <div className="card-body bg-orange-200 text-center">
+                    <h2 className="text-center text-xl font-semibold font-serif ">
+                      {pandaTeddyBear.name}
+                    </h2>
+                    <p className="text-center font-sans flex justify-center items-center font-semibold ">
+                      Price:
+                      <FaDollarSign className="" />
+                      {pandaTeddyBear.price}
+                    </p>
+                    <Rating
+                      className="text-ceneter justify-center my-2"
+                      name="half-rating-read"
+                      defaultValue={pandaTeddyBear.rating}
+                      precision={0.5}
+                      readOnly
+                    ></Rating>
+                    <div className="">
+                      <button className="inline-flex rounded-full items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium">
+                        View Details
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </TabPanel>
-      </Tabs>
+              ))}
+            </div>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
